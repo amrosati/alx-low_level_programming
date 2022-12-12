34edef08408063f92cdce58a5485d75f9eaf3aa2
Amro Satti <amrsatti98@gmail.com>
2022-12-13 01:56:21 +0200

    fixed data type

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char n = '0';
+	int n = 48;
 
-	for (; n <= '9'; n++)
+	for (; n <= 57; n++)
 	{
-		putchar(n);
-		if (n != '9')
+		putchar((char) n);
+		if (n != 57)
 		{
 			putchar(',');
 			putchar(' ');
