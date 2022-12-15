18c64fa6f0d83e84509986608869d29ff472914b
Amro Satti <amrsatti98@gmail.com>
2022-12-16 01:11:22 +0200

    fixed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,10 +10,14 @@
  */
 int largest_number(int a, int b, int c)
 {
+	int largest;
+
 	if (a > b && a > c)
-		return (a);
+		largest = a;
 	else if (b > a && b > c)
-		return (b);
+		largest = b;
 	else
-		return (c);
+		largest = c;
+
+	return (largest);
 }
