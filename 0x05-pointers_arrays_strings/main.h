8b0fe0902e2edbc2e6a175e3764a250cfbd37894
Amro Satti <amrsatti98@gmail.com>
2022-12-20 05:10:07 +0200

    reverse string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
+void swap(char *a, char *b);
 
 #endif /* MAIN_H */
