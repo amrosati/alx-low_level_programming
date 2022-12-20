7ac3a541a73a9fa80bf6715d364db4fab0c2d297
Amro Satti <amrsatti98@gmail.com>
2022-12-20 05:31:59 +0200

    fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,10 @@
  */
 void puts2(char *str)
 {
+	int l = _strlen(str);
 	int i = 0;
 
-	for (; *(str + i) != '\0'; i += 2)
+	for (; i < l; i += 2)
 	{
 		if (*(str + i) == NULL)
 			break;
@@ -18,4 +19,21 @@ void puts2(char *str)
 		_putchar(*(str + i));
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - get length of a string
+ * @s: string to get its length
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	int l = 0;
+
+	for (; *(s + i) != '\0'; i++)
+		l++;
+
+	return (l);
 }
