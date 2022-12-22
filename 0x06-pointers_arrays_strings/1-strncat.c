0333139c2429cdabc15f71a350eda7ac12de9b26
Amro Satti <amrsatti98@gmail.com>
2022-12-22 10:05:32 +0200

    fixed bug

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @src: String to concatenate from
  * @n: Number of bytes to concatenate from `src`
  *
- * Description: Similar to _strcat, except that 
+ * Description: Similar to _strcat, except that
  * it will use at most `n` bytes from `src`, and `src` does not need
  * to be null-terminated if it contains `n` or more bytes.
  *
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (l = 0, destlen = 0; *(dest + l) != '\0'; l++)
 		destlen++;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
 		*(dest + (destlen + i)) = *(src + i);
 	*(dest + (destlen + i)) = '\0';
 
