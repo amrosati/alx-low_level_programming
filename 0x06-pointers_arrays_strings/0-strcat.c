c93f7936aae35e6e299f254b4d58a4279943a732
Amro Satti <amrsatti98@gmail.com>
2022-12-22 08:50:06 +0200

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,30 +13,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dstlen = _strlen(dest);
-	int srclen = _strlen(src);
-	int n = dstlen + srclen;
-	int i;
+	int dstlen = 0;
+	int i = 0;
+	int l = 0;
 
-	*(dest + dstlen++) = *src;
-	for (i = 1; i <= n; i++)
-		*(dest + dstlen++) = *(src + i);
+	for (; *(dest + l) != '\0'; l++)
+		dstlen++;
+
+	for (; *(src + i) != '\0'; i++)
+		*(dest + (dstlen + i)) = *(src + i);
+	if (*(src + i) == '\0')
+		*(dest + (dstlen + i)) = *(src + i);
 
 	return (dest);
-}
-
-/**
- * strlen - Calculate length of string excludeing terminating null byte
- * @str: string to compute
- *
- * Return: length of string
- */
-int _strlen(char *str)
-{
-	int i, l;
-
-	for (i = 0, l = 0; *(str + i) != '\0'; i++)
-		l++;
-
-	return (l);
 }
