e1b588aca38d8ebda98d309364bb02c8e07484c7
Amro Satti <amrsatti98@gmail.com>
2022-12-23 23:26:23 +0200

    task 100

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,16 +1,17 @@
 #ifndef MAIN_H
-#define MAIN_H 
+#define MAIN_H
 
 /* Functions Prototypes */
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-void swap(int *num1, int *num2);
+int _putchar(char);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+void reverse_array(int *, int);
+void swap(int *, int *);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
 
 #endif /* MAIN_H */
