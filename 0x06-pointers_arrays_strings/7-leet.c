9cc52c3e4c9d965e33df19703b62796dfe4efa37
Amro Satti <amrsatti98@gmail.com>
2022-12-23 05:07:01 +0200

    fixed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,9 +12,16 @@ char *leet(char *str)
 	char leet[] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[i++])
+	{
 		for (ii = 0; ii <= 7; ii++)
-			if (str[i] == leet[ii] || (str[i] - 32) == leet[ii])
+		{
+			if (str[i] == leet[ii] ||
+				       (str[i] - 32) == leet[ii])
+			{
 				str[i] = ii + 'O';
+			}
+		}
+	}
 
 	return (str);
 }
