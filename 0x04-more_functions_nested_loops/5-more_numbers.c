fce4c47c9611d14f5b63956f1ee47f24d68016e2
Amro Satti <amrsatti98@gmail.com>
2022-12-25 20:10:36 +0200

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-				_putchar(j + '0');
-			else
-			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
+			if (j > 9)
+				_putchar(49);
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
