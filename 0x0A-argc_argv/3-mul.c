6686a4ffbdf58867f4104b67f0a89729389d7352
Amro Satti <amrsatti98@gmail.com>
2023-01-09 23:53:05 +0200

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two given numbers
+ * @argc: number of args
+ * @argv: args array
+ *
+ * Return: 0 (on success)
+ */
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	res = atoi(argv[1]);
+	res *= atoi(argv[2]);
+
+	printf("%d\n", res);
+
+	return (0);
+}
