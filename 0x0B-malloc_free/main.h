68c438315c23a83068255bc4660d96e29470cb28
Amro Satti <amrsatti98@gmail.com>
2023-01-11 11:28:20 +0200

    task 0

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Libraries Uesed */
+#include <stdlib.h>
+
+/* Functions Prototypes */
+int _putchar(char);
+char *create_array(unsigned int, char);
+
+#endif /* MAIN_H */
