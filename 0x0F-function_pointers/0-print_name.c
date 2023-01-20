c2f23b10007bec986a41f1b14848d42b8cbb3182
Amro Satti <amrsatti98@gmail.com>
2023-01-20 08:42:00 +0200

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - callback function to print a name
+ * @name: name to print
+ * @f: function to call
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef _FUNC_PTR_H_
+#define _FUNC_PTR_H_
+
+/* Functions Prototypes */
+void print_name(char *, void (*f)(char *));
+
+#endif
