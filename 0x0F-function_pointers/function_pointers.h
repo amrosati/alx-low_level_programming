68573a02f3e33dcf061eeb7fe39b0cf8c515ef17
Amro Satti <amrsatti98@gmail.com>
2023-01-20 09:29:06 +0200

    task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
 #ifndef _FUNC_PTR_H_
 #define _FUNC_PTR_H_
 
+/* Include */
+#include <stddef.h>
+#include <stdlib.h>
+
 /* Functions Prototypes */
 void print_name(char *, void (*f)(char *));
+void array_iterator(int *, size_t, void (*action)(int));
 
 #endif
