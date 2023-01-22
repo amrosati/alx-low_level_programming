dc757a6012e3031b13e560520110fa4378d4773a
Amro Satti <amrsatti98@gmail.com>
2023-01-22 03:08:34 +0200

    task 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef _VARIADIC_FUNC_H_
+#define _VARIADIC_FUNC_H_
+
+/* Include */
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* Functions Prototypes */
+int sum_them_all(const unsigned int, ...);
+
+#endif /* _VARIADIC_FUNC_H_ */
