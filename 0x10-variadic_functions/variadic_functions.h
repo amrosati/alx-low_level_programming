311fc71f0c0e8ada13d260a334b8dadfefe01cb7
Amro Satti <amrsatti98@gmail.com>
2023-01-22 06:49:50 +0200

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,5 +10,7 @@ int _putchar(char);
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int, ...);
+void print_all(const char * const, ...);
+void print_sep(char, int);
 
 #endif /* _VARIADIC_FUNC_H_ */
