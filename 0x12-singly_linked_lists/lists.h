7ca934b88f2f2c6ff498b4f17d62af40b98977f6
Amro Satti <amrsatti98@gmail.com>
2023-01-26 21:47:00 +0200

    solved task 0 of printing a linked list recursively

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Data Structures */
+
+/**
+ * struct list_s - singly linked list
+ * @str: malloc'ed string
+ * @len: length of the string
+ * @next: pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Functions Prototypes */
+int _putchar(char);
+size_t print_list(const list_t *);
+
+#endif /* _LISTS_H_ */
