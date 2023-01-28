0a8582b4f7ea1eab9d6234f73cba185b9ecf44e3
Amro Satti <amrsatti98@gmail.com>
2023-01-28 04:15:10 +0200

    fu** the checker

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,50 +9,29 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *cursor = *head;
+	list_t *cursor = NULL;
 	list_t *new_node = NULL;
-
-	new_node = add_node(&cursor, str);
-	if (new_node == NULL)
-		return (NULL);
-
-	if (cursor == NULL)
-	{
-		cursor = new_node;
-
-		return (new_node);
-	}
-
-	while (cursor->next != NULL)
-		cursor = cursor->next;
-
-	cursor->next = new_node;
-
-	return (new_node);
-}
-
-/**
- * add_node - adds a node at the beginning of a list_t list
- * @head: ponter to a pointer to the head of a list
- * @str: string to be duplicated as data to new node
- *
- * Return: address of new node (on success)
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Initialize data */
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
-
-	/* Insert into list */
 	new_node->next = NULL;
-	*head = new_node;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		cursor = *head;
+		while (cursor->next != NULL)
+			cursor = cursor->next;
+
+		cursor->next = new_node;
+	}
 
 	return (new_node);
 }
