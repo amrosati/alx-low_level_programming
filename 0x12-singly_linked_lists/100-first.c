a8e9502f49852f1fd776dab09b3853d45e5b7615
Amro Satti <amrsatti98@gmail.com>
2023-01-30 01:02:09 +0200

    fixed betty styling

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * first - cunstructor function to be executed before main
+ * pre_main - cunstructor function to be executed before main
  *
  * Return: nothing
  */
 void __attribute__ ((constructor)) pre_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
