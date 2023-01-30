df450774ee10565af91217a1dacb2fdf2b21b2cf
Amro Satti <amrsatti98@gmail.com>
2023-01-30 14:46:19 +0200

    solved task 3 and 4 and tested the code

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,5 +24,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *);
 size_t listint_len(const listint_t *);
 listint_t *add_nodeint(listint_t **, const int);
+listint_t *add_nodeint_end(listint_t **, const int);
+void free_listint(listint_t *);
 
 #endif /* _LISTS_H_ */
