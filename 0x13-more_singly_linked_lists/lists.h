4ccbdbb78f06f96d2f7ff4600fb319f57859d602
Amro Satti <amrsatti98@gmail.com>
2023-01-30 13:39:55 +0200

    created 0x13 project directory./ created readme file solved task 0

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/** Data Structures **/
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: pointer to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/** Functions Prototypes **/
+size_t print_listint(const listint_t *);
+
+#endif /* _LISTS_H_ */
