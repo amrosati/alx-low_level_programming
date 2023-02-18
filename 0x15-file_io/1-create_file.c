e5990f985e43235164809894521857e7ab0f744e
Amro Satti <amrsatti98@gmail.com>
2023-02-18 14:42:18 +0200

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: content of the file
+ *
+ * Return: 1 on success, -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, text_len;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	text_len = strlen(text_content);
+
+	if (write(fd, text_content, text_len) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,11 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 /* Functions Prototypes */
 int _putchar(char);
 ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
 
 #endif /* _MAIN_H_ */
