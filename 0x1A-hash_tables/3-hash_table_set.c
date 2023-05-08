6d7dc84783f8df88bd01028a99ff40479ac9c647
Amro Satti <amrsatti98@gmail.com>
2023-05-08 14:30:38 +0300

    fixed errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,13 +10,15 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
+	hash_node_t *new_node, *tmp;
 	unsigned long int index;
 
 	if (!ht)
 		return (0);
-	if (key == NULL)
+	if (key == NULL || strcmp(key, "") == 0)
 		return (0);
+
+	index = key_index((unsigned char *) key, ht->size);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
@@ -26,14 +28,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	new_node->next = NULL;
 
-	index = key_index((unsigned char *) key, ht->size);
-
 	if (ht->array[index] == NULL)
 		ht->array[index] = new_node;
 	else
 	{
+		for (tmp = ht->array[index]; tmp; tmp = tmp->next)
+			if (strcmp(key, tmp->key) == 0)
+			{
+				free(new_node);
+				free(tmp->value);
+				tmp->value = strdup(value);
+				return (1);
+			}
+
 		new_node->next = ht->array[index];
-		(ht->array[index])->next = new_node;
+		ht->array[index] = new_node;
 	}
 
 	return (1);
