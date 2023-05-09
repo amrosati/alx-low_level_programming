e7f3451238cecbfda0747e5ab47d7aadf1aadc28
Amro Satti <amrsatti98@gmail.com>
2023-05-09 11:43:16 +0300

    task 5 and 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ *
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+		if (ht->array[i])
+			free_list(ht->array[i]);
+
+	free(ht->array);
+	free(ht);
+}
+
+/**
+ * free_list - frees a linked list
+ * @list: list
+ *
+ * Return: nothing
+ */
+void free_list(hash_node_t *list)
+{
+	if (!list)
+		return;
+
+	free_list(list->next);
+	free(list->key);
+	free(list->value);
+	free(list);
+}
