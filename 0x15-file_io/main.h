4e106062df81133b140dfdd38f867f6ae6dd3ba4
Amro Satti <amrsatti98@gmail.com>
2023-07-15 06:47:44 +0300

    task 0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/* Functions Prototypes */
+int _putchar(char);
+ssize_t read_textfile(const char *, size_t);
+
+
+#endif /* __MAIN_H__ */
