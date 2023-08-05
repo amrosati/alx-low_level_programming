d402c5fcf26b792e6423c8837fd731a4665eba0d
Amro Satti <amrsatti98@gmail.com>
2023-08-05 13:48:25 +0300

    task 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Functions Prototype */
+int _putchar(char);
+unsigned int binary_to_uint(const char *b);
+
+
+#endif /* __MAIN_H__ */
