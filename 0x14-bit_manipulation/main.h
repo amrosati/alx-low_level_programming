685dd08b1609cee647d7142048377dab6b0940aa
Amro Satti <amrsatti98@gmail.com>
2023-08-06 18:41:40 +0300

    task 4

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,6 +11,7 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
 
 
 #endif /* __MAIN_H__ */
