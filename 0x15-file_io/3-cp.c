41974e518c464d0d2205a6ddb66cf0f4142518fa
Amro Satti <amrsatti98@gmail.com>
2023-08-08 14:28:25 +0300

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,28 +13,52 @@
 int main(int argc, char **argv)
 {
 	int fd_from, fd_to;
-	char *buf;
-	ssize_t nr, nw;
+	char buf[1024];
+	ssize_t nr;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		err("Usage: cp file_from file_to", NULL, 0, 97);
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		err("Error: Can't read from file ", argv[1], 0, 98);
 
 	fd_to = creat(argv[2], 0664);
 	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		err("Error: Can't write to ", argv[2], 0, 99);
 
 	while ((nr = read(fd_from, buf, 1024)))
-		write(fd_to, buf, 
+	{
+		if (nr == -1)
+			err("Error: Can't read from file ", argv[1], 0, 98);
+
+		if (write(fd_to, buf, nr) == -1)
+			err("Error: Can't write to ", argv[2], 0, 99);
+	}
+
+	if (close(fd_from))
+		err("Error: Can't close fd ", NULL, fd_from, 100);
+	if (close(fd_to))
+		err("Error: Can't close fd ", NULL, fd_to, 100);
+
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * err - Prints the error msg and exits with given code
+ * @msg: The specified msg
+ * @filename: The file name
+ * @fd: File descriptor if the error on closing
+ * @code: The exit code
+ */
+void err(char *msg, char *filename, int fd, int code)
+{
+	if (!filename && !fd)
+		dprintf(STDERR_FILENO, "%s\n", msg);
+	else if (fd)
+		dprintf(STDERR_FILENO, "%s%d\n", msg, fd);
+	else
+		dprintf(STDERR_FILENO, "%s%s\n", msg, filename);
+
+	exit(code);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,6 +14,7 @@ int _putchar(char);
 ssize_t read_textfile(const char *, size_t);
 int create_file(const char *, char *);
 int append_text_to_file(const char *, char *);
+void err(char *, char *, int, int);
 
 
 #endif /* __MAIN_H__ */
