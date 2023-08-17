1da1ad774d164155975db39d70616a3f09fe79c7
Amro Satti <amrsatti98@gmail.com>
2023-08-17 11:47:20 +0300

    task 0

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef __LISTS_H__
+#define __LISTS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - Doubly linked list
+ * @n: integer
+ * @prev: previous node
+ * @next: next node
+ *
+ * Description: Doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/* Functions Prototypes */
+size_t print_dlistint(const dlistint_t *);
+
+
+#endif /* __LISTS_H__ */
