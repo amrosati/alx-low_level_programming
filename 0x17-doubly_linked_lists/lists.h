5366e1e6003bfe3467beced07b0ea37a989698c1
Amro Satti <amrsatti98@gmail.com>
2023-08-19 15:19:36 +0300

    tasks 5, 6, and 7

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -26,6 +26,9 @@ size_t dlistint_len(const dlistint_t *);
 dlistint_t *add_dnodeint(dlistint_t **, const int);
 dlistint_t *add_dnodeint_end(dlistint_t **, const int);
 void free_dlistint(dlistint_t *);
+dlistint_t *get_dnodeint_at_index(dlistint_t *, unsigned int);
+int sum_dlistint(dlistint_t *);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **, unsigned int, int);
 
 
 #endif /* __LISTS_H__ */
